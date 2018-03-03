fe84d724d4700ead01fa8d70c9a884f87c717745
zalegzl <zalegzl@gmail.com>
2018-03-03 17:24:18 +0800

    update

--- src/models/index.js
+++ src/models/index.js
@@ -1,5 +1,17 @@
 const mongoose = require('mongoose')
-mongoose.connect('mongodb://localhost/zalegzl')
+const fs = require('fs')
+const path = require('path')
+
+const info = JSON.parse(
+  fs.readFileSync(path.resolve(__dirname, '../privateInfo.json')).toString()
+)
+
+const account = info.adminAccount
+const pwd = info.adminPassword
+
+mongoose.connect(
+  `mongodb://${account}:${pwd}@119.29.145.190:27017/zalegzl?authSource=admin`
+)
 
 const db = mongoose.connection
 db.on('error', console.error.bind(console, 'mongodb connection error:'))
